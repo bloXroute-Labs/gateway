1dfd39af6f700343532ef3564f9b82c1d34ab5e8
danielle <danielle.machpud@bloxroute.com>
2021-11-09 17:13:05 +0200

    changed ecr repo name

--- build-gateway.sh
+++ build-gateway.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-IMAGE=033969152235.dkr.ecr.us-east-1.amazonaws.com/bloxroute-gateway-go:${1:-latest}
+IMAGE=033969152235.dkr.ecr.us-east-1.amazonaws.comgateway-go:${1:-latest}
 echo "Building container... $IMAGE"
 docker build . -f Dockerfile-gateway --rm=true -t $IMAGE
