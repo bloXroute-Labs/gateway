3be5afb32cbd6aa0c10e00d8a7996a0ecf490d40
rsukhodolskyi <roman.sukhodolskyi@bloxroute.com>
2023-07-06 11:39:42 +0300

    Publish release v2.126.11

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-IMAGE=bloxroute/gateway:${1:-latest}
+IMAGE=bloxroute/bxgateway-go-release-candidate:${1:-latest}
 # extract tag and keep in file (can't be done in docker). Used by Makefile
-tag=`git describe --tags --always --dirty --match='v2*' 2> /dev/null`
+tag=`git describe --tags --always --dirty --match=v2* 2> /dev/null`
 echo $tag > .gittag
 echo "Building container... $IMAGE"
-docker build --no-cache . -f Dockerfile --rm=true --platform linux/x86_64 -t $IMAGE
+docker build  . -f Dockerfile --rm=true --platform linux/x86_64 -t $IMAGE
+docker tag $IMAGE 033969152235.dkr.ecr.us-east-1.amazonaws.com/bloxroute-gateway-go:${1:-latest}
+docker tag $IMAGE bloxroute/bxgateway-go-release-candidate:${1:-latest}
 rm .gittag
