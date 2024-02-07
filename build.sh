f1ddc8443be1c5f3b0ceff4449a60f32c432e149
Oleksandr Iozhytsia <iojitsa@gmail.com>
2024-02-07 15:53:10 +0100

    Add possibility to set build platform in build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 IMAGE=bloxroute/gateway:${1:-latest}
+# we should build public version for at least two platforms:
+# linux/x86_64 (default) and linux/arm64/v8 ${2:linux/x86_64}
+PLATFORM=${2:-linux/x86_64}
 # extract tag and keep in file (can't be done in docker). Used by Makefile
 tag=`git describe --tags --always --dirty --match='v2*' 2> /dev/null`
 echo $tag > .gittag
 echo "Building container... $IMAGE"
-docker build --no-cache . -f Dockerfile --rm=true --platform linux/x86_64 -t $IMAGE
+docker build --no-cache . -f Dockerfile --rm=true --platform $PLATFORM -t $IMAGE
 rm .gittag
