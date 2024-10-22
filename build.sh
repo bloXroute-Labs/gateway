d9d0b55c76fe4d896bb44f465c5a0c015b82af12
leonbiton1 <leon.biton@bloxroute.com>
2024-10-22 12:04:04 +0300

    add-portable-build

--- build.sh
+++ build.sh
@@ -3,9 +3,16 @@ IMAGE=bloxroute/bloxroute-gateway-go:${1:-latest}
 # we should build public version for at least two platforms:
 # linux/x86_64 (default) and linux/arm64/v8
 PLATFORM=${2:-linux/x86_64}
+PORTABLE=${3:-false}
 # extract tag and keep in file (can't be done in docker). Used by Makefile
 tag=`git describe --tags --always --dirty --match='v2*' 2> /dev/null`
 echo $tag > .gittag
-echo "Building container... $IMAGE"
-docker build --no-cache . -f Dockerfile --rm=true --platform $PLATFORM -t $IMAGE
+if [ "$PORTABLE" = "true" ]; then
+    echo "Building portable container with BLST_PORTABLE... $IMAGE-portable"
+    docker build --no-cache . -f Dockerfile --rm=true --platform linux/x86_64 --build-arg PORTABLE=true -t $IMAGE
+else
+    echo "Building container... $IMAGE"
+    docker build --no-cache . -f Dockerfile --rm=true --platform $PLATFORM -t $IMAGE
+fi
+
 rm .gittag
